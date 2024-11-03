ac5cabd596badfdf8f7d788fa95b31d337118d85
Adam Kovari <kovariadam@gmail.com>
2024-11-03 20:21:11 +0100

    Add project files.

---
+++ vcpkg_installed/x64-windows/share/antlr4-runtime/antlr4-targets-debug.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "antlr4_shared" for configuration "Debug"
+set_property(TARGET antlr4_shared APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(antlr4_shared PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/debug/lib/antlr4-runtime.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/debug/bin/antlr4-runtime.dll"
+  )
+
+list(APPEND _cmake_import_check_targets antlr4_shared )
+list(APPEND _cmake_import_check_files_for_antlr4_shared "${_IMPORT_PREFIX}/debug/lib/antlr4-runtime.lib" "${_IMPORT_PREFIX}/debug/bin/antlr4-runtime.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
---
+++ yonac-llvm.cpp
@@ -0,0 +1,12 @@
+﻿// yonac-llvm.cpp : Defines the entry point for the application.
+//
+
+#include "yonac-llvm.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
