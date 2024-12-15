b3bc23285e4fee0e1fc3b062476d3b1dfeabdfbc
akovari <a.kovari@digital-science.com>
2024-12-15 18:25:24 +0100

    gha presets

--- test/ast.cpp
+++ test/ast.cpp
@@ -3,25 +3,28 @@
 //
 #include <iostream>
 #include <gtest/gtest.h>
+#include <boost/log/utility/setup/file.hpp>
 
 #include "utils.h"
 
 using namespace std;
 using namespace yona;
 
-class AstTest : public testing::TestWithParam<tuple<string, string, unsigned int>> {};
+
+
+class AstTest : public testing::TestWithParam<tuple<string, string, string, unsigned int>> {};
 
 TEST_P(AstTest, MyTest)
 {
     auto param = GetParam();
-    auto input = get<0>(param);
-    auto expected = get<1>(param);
-    auto expected_errors = get<2>(param);
+    auto input = get<1>(param);
+    auto expected = get<2>(param);
+    auto expected_errors = get<3>(param);
 
     stringstream ss(input);
     auto result = parse_input(ss);
 
-    auto node = result.node;
+    // auto node = result.node;
     auto type = result.type;
     auto type_ctx = result.type_ctx;
 
@@ -33,6 +36,20 @@ TEST_P(AstTest, MyTest)
 }
 
 INSTANTIATE_TEST_SUITE_P(SyntaxTests, AstTest, testing::Values(
-    make_tuple("1+1", "2", 0),
-    make_tuple("1+'1'", "2", 1)
-    ));
+    make_tuple("correct_addition_of_ints", "1+1", "2", 0),
+    make_tuple("failed_addition_of_int_with_char", "1+'1'", "2", 1)
+    ),
+    [](const testing::TestParamInfo<AstTest::ParamType>& info) {
+      return get<0>(info.param);
+    });
+
+void init_logging()
+{
+    boost::log::add_file_log("tests.log");
+}
+
+int main(int argc, char **argv) {
+    init_logging();
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
