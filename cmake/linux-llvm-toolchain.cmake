2fc0f68ba6c83fa45de0f949137b100ac5db6f75
akovari <a.kovari@digital-science.com>
2025-07-12 16:01:36 +0200

    use llvm toolchain on linux

---
+++ cmake/linux-llvm-toolchain.cmake
@@ -0,0 +1,21 @@
+# Toolchain file for building with LLVM on Linux (Ubuntu/Debian)
+# This is used by GitHub Actions and developers who want to use LLVM instead of GCC
+
+# Set the compilers - using versioned binaries as installed by apt
+set(CMAKE_C_COMPILER "/usr/bin/clang-20" CACHE FILEPATH "C compiler")
+set(CMAKE_CXX_COMPILER "/usr/bin/clang++-20" CACHE FILEPATH "C++ compiler")
+
+# Use LLVM's libc++ instead of GNU libstdc++
+set(CMAKE_CXX_FLAGS "-stdlib=libc++" CACHE STRING "")
+
+# Use lld as the linker for faster linking
+set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=lld -stdlib=libc++" CACHE STRING "")
+set(CMAKE_SHARED_LINKER_FLAGS "-fuse-ld=lld -stdlib=libc++" CACHE STRING "")
+set(CMAKE_MODULE_LINKER_FLAGS "-fuse-ld=lld -stdlib=libc++" CACHE STRING "")
+
+# Ensure we link against libc++abi as well
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lc++abi" CACHE STRING "" FORCE)
+set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -lc++abi" CACHE STRING "" FORCE)
+
+# Set RPATH to find libc++ at runtime if it's not in standard locations
+set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
